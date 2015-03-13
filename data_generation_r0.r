a5f992cae5e39f34515a6801ca234b4152c54f28
DanRunfola <dan@danrunfola.com>
2015-03-13 15:27:37 -0400

    Random Generation v.2

--- data_generation_r0.r
+++ data_generation_r0.r
@@ -15,3 +15,58 @@ KFW_poly = readShapePoly(KFW_shp)
 #Remove the "Ha" from the area field, so we can interpret it as a numeric value.
 KFW_poly@data$terrai_are <- sub('Ha', '',KFW_poly@data$terrai_are)
 
+#Change it to a numeric value (it still technically thinks it's a string)
+KFW_poly@data$terrai_are <- as.double(KFW_poly@data$terrai_are)
+
+#Rename the field so we can reference it easier, later.
+names(KFW_poly@data)[names(KFW_poly@data) == 'terrai_are'] <- "CommunityArea_Ha" 
+
+#For now, create an imaginary NDVI value for each year between 1981 and 2013.
+#First, initialize a counter to track the "year" we are creating data for.
+year = 1981
+
+#Now, make a loop that creates "dummy" data for each year.
+#Note, to make this at least vaguely real we will be correlating
+#each year with the previous year's information, rather than simply
+#generating random data for each year.
+
+#Generate year 1 randomly (because there isn't anything before it to go on):
+#First, we create the variable name.  This means the column will end up "NDVI_1981"
+col_name = paste("NDVI_",year)
+
+#Now, we populate the column with a random value between 0 and 1- there are many ways to do this,
+#The below gives you the most explicit control (runif(1) does a much simpler version of this):
+#First, we seed it with random value between 0 and 10,000:
+KFW_poly@data[col_name] = sample(10000, size=nrow(KFW_poly@data), replace=TRUE)
+#Then we divide by the total maximum value (in our case, 10,000):
+KFW_poly@data[col_name] = KFW_poly@data[col_name] / 10000
+
+#We generated the first year, so we go ahead and move to the next one:
+year = year + 1
+
+#From here out, we are going to generate the next year based on the previous one,
+#based on a truncated normal distribution.
+#Note NDVI can never exceed 1 or decrease below 0 in our true dataset
+#so those restrictions are enforced here.
+
+while (year <= 2014)
+{
+  col_name = paste("NDVI_",year)
+  last_year = year - 1
+  last_year_name = paste("NDVI_",last_year)
+  
+  #Declare the new column to be similar to last year, based on an entirely arbitrary
+  #normal distribution centered on 0 to at least allow for *some* possible shocks.
+  KFW_poly@data[col_name] = KFW_poly@data[last_year_name] + rnorm(nrow(KFW_poly@data),mean=0,sd=0.1)
+  
+  #Replace values greater than 1 with 1, less than 0 with 0
+  KFW_poly@data[col_name][KFW_poly@data[col_name]<0] <- 0
+  KFW_poly@data[col_name][KFW_poly@data[col_name]>1] <- 1
+  
+  #Go to the next year and do it again!
+  year = year + 1
+  
+}
+
+#Assign every unit of observation a random "Entry Year" and "Accepted" year.
+
