df6fc23aa7e3898ccc3885409b905ca607f4678e
DanRunfola <dan@danrunfola.com>
2015-03-13 14:50:29 -0400

    KFW v1

--- data_generation_r0.r
+++ data_generation_r0.r
@@ -1,9 +1,15 @@
-#Test Git with Rachel for KFW
+#Data Generation Script for KFW
 
-x = 1
-print(x)
+#This script will eventually load all data into a primary database for analysis
+#In the near term, this script will generate random data for use in analysis.
 
-x=5
-print(x)
+#Libraries
+library(sp) #spatial data handling
+library(GISTools)
 
-print("Different")
+#First, load the shapefile with all communities that were at least approved.
+KFW_shp = "/home/aiddata/Desktop/R_Repo/KFW/Input_Data/Matched_Indigenous_Lands.shp"
+KFW_poly = readShapePoly(KFW_shp)
+
+#Remove the "Ha" from the area field, so we can interpret it as a numeric value.
+KFW_poly@data$terrai_are <- sub('Ha', '',KFW_poly@data$terrai_are)
